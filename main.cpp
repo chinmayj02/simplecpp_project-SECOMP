9a3d43fcffaa1ef30d6eb179598f347146890010
Chinmay Joshi <chinmayujoshi@gmail.com>
2022-01-06 16:18:34 +0530

    credits and controls updated

--- main.cpp
+++ main.cpp
@@ -9,26 +9,40 @@ void prePlay();
 
 void clickPos()
 {
-    float clickpos[2]={getClick()/65536,getClick()%65536};
+    float clickpos[2]={getClick()/65536, getClick()%65536};
     cout<<clickpos[0]<<"\t"<<clickpos[1]<<endl;
 }
 
 
 void play()
 {
-    int car_body_start[2]={0,0},car_wheel1_start[2]={36,177},car_wheel2_start[2]={116,177};
+    int car_body_start[2]={0, 0},car_wheel1_start[2]={36, 177},car_wheel2_start[2]={116, 177};
     prePlay();
-    Line l1(0,200,600,200);
-    Line l2(0,196,600,196);
-    Line l3(0,194,600,194);
-    l3.setColor(COLOR(255,255,255));
-    Line l4(0,190,600,190);
-    Line l5(0,186,600,186);
-    double sides[13][2]={16,148,16,169,30,169,55,171,90,172,118,172,142,177,133,157,114,154,96,140,50,140,33,157,16,148};
-    Polygon b(car_body_start[0],car_body_start[1],sides,13);
+    Line l1(0, 200, 600, 200);
+    Line l2(0, 196, 600, 196);
+    Line l3(0, 194, 600, 194);
+    l3.setColor(COLOR(255, 255, 255));
+    Line l4(0, 190, 600, 190);
+    Line l5(0, 186, 600, 186);
+    double sides[13][2]={
+                         (016, 148),
+                         (016, 169),
+                         (030, 169),
+                         (055, 171),
+                         (090, 172),
+                         (118, 172),
+                         (142, 177),
+                         (133, 157),
+                         (114, 154),
+                         (096, 140),
+                         (050, 140),
+                         (033, 157),
+                         (016, 148)
+                        };
+    Polygon b(car_body_start[0], car_body_start[1], sides,13);
     b.setFill();
-    Circle w1(car_wheel1_start[0],car_wheel1_start[1],10);
-    Circle w2(car_wheel2_start[0],car_wheel2_start[1],10);
+    Circle w1(car_wheel1_start[0], car_wheel1_start[1], 10);
+    Circle w2(car_wheel2_start[0], car_wheel2_start[1], 10);
     w1.setFill();
     w2.setFill();
    // while(1)clickPos();
@@ -36,22 +50,22 @@ void play()
     int k=1;
     while( k!=0)
     {
-    for(int m=0;m<=50;m++)
-    {
-    b.moveTo(car_body_start[0]+=m,0);
-    w1.moveTo(car_wheel1_start[0]+=m,177);
-    w2.moveTo(car_wheel2_start[0]+=m,177);
-    }
-    wait(2);
-    k--;
+        for(int m=0;m<=50;m++)
+        {
+            b.moveTo(car_body_start[0]+=m, 0);
+            w1.moveTo(car_wheel1_start[0]+=m, 177);
+            w2.moveTo(car_wheel2_start[0]+=m, 177);
+        }
+        wait(2);
+        k--;
     }
     while(1)clickPos();
 }
 
 void menu()
 {
-    Rectangle bg(300,300,600,600);
-    bg.setColor(COLOR(48,48,48));
+    Rectangle bg(300, 300, 600, 600);
+    bg.setColor(COLOR(48, 48, 48));
     bg.setFill();
     char ch;
     XEvent e1;
@@ -72,13 +86,15 @@ void menu()
                 break;
         case 50:XEvent e2;
                 {
-                        Text t1(300, 220, "Controls: ");
-                        Text t2(300, 260, "Goal: ");
-                        Text t3(300, 300, "-");
-                        Text t4(300, 340, "-");
-                        Text t5(300, 380, "-");
-                        Text t6(300, 420, "-");
-                        Text t7(300, 460, "Press any key to go back!");
+                        Text t1(300, 150, "Controls: ");
+                        Text c1(300, 190, "Use mouse to hover over the options.");
+                        Text c2(300, 230, "Left mouse click to select your answer.");
+                        Text t2(300, 270, "Goal: ");
+                        Text t3(300, 310, "This is a very basic traffic test.");
+                        Text t4(300, 350, "Total of 5 multiple choice questions will be asked.");
+                        Text t5(300, 390, "Score will be calculated based on");
+                        Text t6(300, 410, "the number of correct answers and the speed of answering");
+                        Text t7(300, 470, "Press any key to go back!");
                         t7.setColor(COLOR(255, 0, 0));
                         nextEvent(e2);
                         int an = charFromEvent(e2);
@@ -91,17 +107,22 @@ void menu()
                             t5.~Text();
                             t6.~Text();
                             t7.~Text();
+                            c1.~Text();
+                            c2.~Text();
                             menu();
                         }
                     }
         case 51:XEvent e3;
                 {
-                        Text t1(300, 220, "CREDITS:");
-                        Text t2(300, 260, "Project Members: ");
-                        Text t3(300, 300, "Chinmay Joshi, Shubham Tendulkar, Vadiraj Inamdar, Vibhav Desai");
-                        Text t4(300, 340, "Teacher Incharge: Prof. Amrita Naik");
-                        Text t5(300, 380, "Press any key to go back!");
-                        t5.setColor(COLOR(255, 0, 0));
+                        Text t1(300, 150, "CREDITS:");
+                        Text t2(300, 190, "Project Members: ");
+                        Text m1(300, 230, "Vibhav Desai (2014XXX)");
+                        Text m2(300, 250, "Vadiraj Inamdar (2014XXX)");
+                        Text m3(300, 270, "Shubham Tendulkar (2014XXX)");
+                        Text m4(300, 290, "Chinmay Joshi (2014006)");
+                        Text t3(300, 330, "Faculty Incharge: Prof. Amrita Naik");
+                        Text t4(300, 380, "Press any key to go back!");
+                        t4.setColor(COLOR(255, 0, 0));
                         nextEvent(e3);
                         int an = charFromEvent(e3);
                         if (an)
@@ -110,7 +131,10 @@ void menu()
                             t2.~Text();
                             t3.~Text();
                             t4.~Text();
-                            t5.~Text();
+                            m2.~Text();
+                            m1.~Text();
+                            m3.~Text();
+                            m4.~Text();
                             menu();
                         }
                     }
@@ -146,13 +170,13 @@ void prePlay()
         Text t1(300, 110, "Few things before starting");
         Text t2(300, 140, "Enter your year of birth");
         Text t3(300, 170, "YYYY: ");
-        Text y(310+(textWidth("YYYY: ")/2)+(2*textHeight()),170,year);
-        Text t4(310+(textWidth("YYYY: ")/2),170," - ");
-        Rectangle dec(310+(textWidth("YYYY: ")/2),170,textHeight(),textHeight());
-        Text t5(305+(textWidth("YYYY: ")/2)+(textHeight()/2)+textWidth("9999")+(2*textHeight()),170," + ");
-        Rectangle inc(305+(textWidth("YYYY: ")/2)+(textHeight()/2)+textWidth("9999")+(2*textHeight()),170,textHeight(),textHeight());
-        Rectangle db(340+(textWidth("YYYY: ")/2)+(textHeight()/2)+textWidth("9999")+(2*textHeight()),170,textWidth("DONE"),textHeight());
-        Text db_t(340+(textWidth("YYYY: ")/2)+(textHeight()/2)+textWidth("9999")+(2*textHeight()),170,"DONE");
+        Text y(310+(textWidth("YYYY: ")/2)+(2*textHeight()), 170, year);
+        Text t4(310+(textWidth("YYYY: ")/2), 170, " - ");
+        Rectangle dec(310+(textWidth("YYYY: ")/2), 170, textHeight(), textHeight());
+        Text t5(305+(textWidth("YYYY: ")/2)+(textHeight()/2)+textWidth("9999")+(2*textHeight()), 170, " + ");
+        Rectangle inc(305+(textWidth("YYYY: ")/2)+(textHeight()/2)+textWidth("9999")+(2*textHeight()), 170, textHeight(), textHeight());
+        Rectangle db(340+(textWidth("YYYY: ")/2)+(textHeight()/2)+textWidth("9999")+(2*textHeight()), 170, textWidth("DONE"), textHeight());
+        Text db_t(340+(textWidth("YYYY: ")/2)+(textHeight()/2)+textWidth("9999")+(2*textHeight()), 170, "DONE");
         while(1)
         {
             XEvent e;
@@ -166,7 +190,7 @@ void prePlay()
                         {
                             y.~Text();
                             year--;
-                            Text y(310+(textWidth("YYYY: ")/2)+(2*textHeight()),170,year);
+                            Text y(310+(textWidth("YYYY: ")/2)+(2*textHeight()), 170, year);
                             while(!getClick())
                             wait(0.5);
                         }
@@ -177,7 +201,7 @@ void prePlay()
                         {
                             y.~Text();
                             year++;
-                            Text y(310+(textWidth("YYYY: ")/2)+(2*textHeight()),170,year);
+                            Text y(310+(textWidth("YYYY: ")/2)+(2*textHeight()), 170, year);
                             while(!getClick())
                             wait(0.5);
                         }
@@ -211,7 +235,7 @@ void prePlay()
         XEvent name;
         {
         int i=0;
-        Text a(300,200,"Type your name: ");
+        Text a(300, 200, "Type your name: ");
         while(1)
         {
             nextEvent(name);
@@ -241,7 +265,7 @@ void prePlay()
 
 int main()
 {
-    initCanvas("DRIVE",600,600);
+    initCanvas("MAIN", 600, 600);
     menu();
     closeCanvas();
     getch();
