eebf99b9d360f25db772ab556a4b0ff8c7519e5a
Chinmay Joshi <chinmayujoshi@gmail.com>
2022-01-06 19:29:40 +0530

    timestamp addes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<simplecpp>
 #include<string>
 #include<ctype.h>
+#include<ctime>
 
 using namespace std;
 
@@ -31,42 +32,38 @@ void play()
     Circle w2(car_wheel2_start[0], car_wheel2_start[1], 10);
     w1.setFill();
     w2.setFill();
-   // while(1)clickPos();
+    double star_sides[5][2];
+    for(int i=0; i<5; i++)
+    {
+        star_sides[i][0] = 5*cos(4*PI/5*i);
+        star_sides[i][1] = 5*sin(4*PI/5*i);
+    }
+    Polygon star1(168, 168, star_sides, 5);
+    Polygon star2(268, 168, star_sides, 5);
+    Polygon star3(368, 168, star_sides, 5);
+    Polygon star4(468, 168, star_sides, 5);
+    Polygon star5(568, 168, star_sides, 5);
+    star1.setColor(COLOR(255, 234, 0));
+    star2.setColor(COLOR(255, 234, 0));
+    star3.setColor(COLOR(255, 234, 0));
+    star4.setColor(COLOR(255, 234, 0));
+    star5.setColor(COLOR(255, 234, 0));
+    star1.setFill();
+    star2.setFill();
+    star3.setFill();
+    star4.setFill();
+    star5.setFill();
+    while(1)
+    {
+        star1.right(5);
+        star2.right(5);
+        star3.right(5);
+        star4.right(5);
+        star5.right(5);
+        wait(0.1);
+    }
 
-   ////star test
-   double star_sides[5][2];
-
-  for(int i=0; i<5; i++)
-  {
-    star_sides[i][0] = 5*cos(4*PI/5*i);
-    star_sides[i][1] = 5*sin(4*PI/5*i);
-  }
-  Polygon star1(168,168,star_sides,5);
-  Polygon star2(268,168,star_sides,5);
-  Polygon star3(368,168,star_sides,5);
-  Polygon star4(468,168,star_sides,5);
-  Polygon star5(568,168,star_sides,5);
-  star1.setColor(COLOR(255, 234, 0));
-  star2.setColor(COLOR(255, 234, 0));
-  star3.setColor(COLOR(255, 234, 0));
-  star4.setColor(COLOR(255, 234, 0));
-  star5.setColor(COLOR(255, 234, 0));
-  star1.setFill();
-  star2.setFill();
-  star3.setFill();
-  star4.setFill();
-  star5.setFill();
-  for(int i=0; i<100; i++)
-  {
-    star1.right(5);
-    star2.right(5);
-    star3.right(5);
-    star4.right(5);
-    star5.right(5);
-    wait(0.1);
-  }
-  wait(4);
-    /*for(int m=0;m<=50;m++)
+  /*for(int m=0;m<=50;m++)
     {
         b.moveTo(car_body_start[0]+=m, 0);
         w1.moveTo(car_wheel1_start[0]+=m, 177);
@@ -179,8 +176,11 @@ void ageRestrict()
 
 void prePlay()
 {
+        time_t now = time(0);
+        char* dt = ctime(&now);
         int year=2000;
         Text t(300, 50, "W E L C O M E !");
+        Text tm(500,20,dt);
         t.setColor(COLOR(255, 0, 0));
         Text t1(300, 110, "Few things before starting");
         Text t2(300, 140, "Enter your year of birth");
